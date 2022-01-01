3a600d878622d435ae2fbd7a4f5d685b8e3b2c24
Repotecc <60607848+Repotecc@users.noreply.github.com>
2022-01-01 16:03:29 +0100

    Update hireusform.php

--- hireusform.php
+++ hireusform.php
@@ -11,12 +11,12 @@ require 'vendor/autoload.php';
 
 // Replace sender@example.com with your "From" address.
 // This address must be verified with Amazon SES.
-$sender = 'info@repotec.com';
+$sender = 'info@repotecc.com';
 $senderName = 'Repotecc';
 
 // Replace recipient@example.com with a "To" address. If your account
 // is still in the sandbox, this address must be verified.
-$recipient = 'ayo6706@gmail.com';
+$recipient = 'ayomide@repotecc.com';
 
 // Replace smtp_username with your Amazon SES SMTP user name.
 $usernameSmtp = 'AKIA3GXMZZ4ETNFBSDOL';
