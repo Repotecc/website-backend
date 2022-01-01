3aaa98390cef668b0fa5a79cdb046b56cbbbe2fe
Repotecc <60607848+Repotecc@users.noreply.github.com>
2022-01-01 15:55:21 +0100

    Update hireusform.php

--- hireusform.php
+++ hireusform.php
@@ -1,52 +1,87 @@
-<?php 
-ini_set("include_path", '/home2/kennysu1/php:' . ini_get("include_path") );
+<?php
 
-error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_STRICT);
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-        
-require_once "Mail.php";
+// If necessary, modify the path in the require statement below to refer to the
+// location of your Composer autoload.php file.
+require 'vendor/autoload.php';
 
-$host = "ssl://mail.repotecc.com";
-$username = "admin@repotecc.com";
-$password = "*******";
-$port = "465";
+// Replace sender@example.com with your "From" address.
+// This address must be verified with Amazon SES.
+$sender = 'info@repotec.com';
+$senderName = 'Repotecc';
 
+// Replace recipient@example.com with a "To" address. If your account
+// is still in the sandbox, this address must be verified.
+$recipient = 'ayo6706@gmail.com';
 
-$first_name = $_POST["first_name"];
-$last_name = $_POST["last_name"];
-$company_name = $_POST["company_name"];
-$work_email = $_POST["work_email"];
-$phone_number = $_POST["phone_number"];
-$country = $_POST["country"];
-$no_of_employees = $_POST["no_of_employees"]; 
-$role = $_POST["role"];
-$to = "info@repotecc.com"; 
+// Replace smtp_username with your Amazon SES SMTP user name.
+$usernameSmtp = 'AKIA3GXMZZ4ETNFBSDOL';
 
-
-$email_from = $work_email;
-$email_subject = "hire us" ;
-$email_body = "First Name: ".$first_name."
-\n Last Name: ".$last_name."\n Company Name: ".$company_name."\n
-Work Email: ".$work_email."\n Phone Number: ".$phone_number."
-\n Country: ".$country."\n No of Employees: ".$no_of_employees."\n
-role: ".$role."";
+// Replace smtp_password with your Amazon SES SMTP password.
+$passwordSmtp = 'BGznd9n00GEC/HQW0ROCwfQs3PQ3YJm3iPJmM+hcwB+K';
 
 
-$email_address = "ayomide@repotecc.com";
-
-$headers = array ('From' => $email_from, 'To' => $to, 'Subject' => $email_subject, 'Reply-To' => $email_address);
-$smtp = Mail::factory('smtp', array ('host' => $host, 'port' => $port, 'auth' => true, 'username' => $username, 'password' => $password));
-$mail = $smtp->send($to, $headers, $email_body);
 
 
-if (PEAR::isError($mail)) {
-echo("<p>" . $mail->getMessage() . "</p>");
 
-} else {
-echo "<script>alert('Form Successfully Submitted');
-        location.href = 'https://repotecc.com';
-    </script>";
-// header("location: https://repotecc.com");
+// Specify a configuration set. If you do not want to use a configuration
+// set, comment or remove the next line.
+$configurationSet = 'ConfigSet';
+
+// If you're using Amazon SES in a region other than US West (Oregon),
+// replace email-smtp.us-west-2.amazonaws.com with the Amazon SES SMTP
+// endpoint in the appropriate region.
+$host = 'email-smtp.eu-west-2.amazonaws.com';
+$port = 587;
+
+// The subject line of the email
+$subject = 'Amazon SES test (SMTP interface accessed using PHP)';
+
+// The plain-text body of the email
+$bodyText =  "Email Test\r\nThis email was sent through the
+    Amazon SES SMTP interface using the PHPMailer class.";
+
+// The HTML-formatted body of the email
+$bodyHtml = '<h1>Email Test</h1>
+    <p>This email was sent through the
+    <a href="https://aws.amazon.com/ses">Amazon SES</a> SMTP
+    interface using the <a href="https://github.com/PHPMailer/PHPMailer">
+    PHPMailer</a> class.</p>';
+
+$mail = new PHPMailer(true);
+
+try {
+    // Specify the SMTP settings.
+    $mail->isSMTP();
+    $mail->setFrom($sender, $senderName);
+    $mail->Username   = $usernameSmtp;
+    $mail->Password   = $passwordSmtp;
+    $mail->Host       = $host;
+    $mail->Port       = $port;
+    $mail->SMTPAuth   = true;
+    $mail->SMTPSecure = 'tls';
+    $mail->addCustomHeader('X-SES-CONFIGURATION-SET', $configurationSet);
+
+    // Specify the message recipients.
+    $mail->addAddress($recipient);
+    // You can also add CC, BCC, and additional To recipients here.
+
+    // Specify the content of the message.
+    $mail->isHTML(true);
+    $mail->Subject    = $subject;
+    $mail->Body       = $bodyHtml;
+    $mail->AltBody    = $bodyText;
+    $mail->Send();
+    echo "Email sent!" , PHP_EOL;
+} catch (phpmailerException $e) {
+    echo "An error occurred. {$e->errorMessage()}", PHP_EOL; //Catch errors from PHPMailer.
+} catch (Exception $e) {
+    echo "Email not sent. {$mail->ErrorInfo}", PHP_EOL; //Catch errors from Amazon SES.
 }
 
+?>
 
