70abaa0148cc7cd07d6c451631329a5d604b2172
repotecc <repotecc@gmail.com>
2022-01-02 22:38:51 +0100

    added smtp
    Co-Authored-By: ayomide onibokun <35877863+ayo6706@users.noreply.github.com>

--- contactform.php
+++ contactform.php
@@ -8,6 +8,46 @@
     $_POST = json_decode($rest_json, true); 
     $errors = array(); 
 
+
+    use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+// If necessary, modify the path in the require statement below to refer to the
+// location of your Composer autoload.php file.
+require 'vendor/autoload.php';
+
+// Replace sender@example.com with your "From" address.
+// This address must be verified with Amazon SES.
+$sender = 'ayomide@repotecc.com';
+$senderName = 'Repotecc';
+
+// Replace recipient@example.com with a "To" address. If your account
+// is still in the sandbox, this address must be verified.
+$recipient = 'info@repotecc.com';
+
+// Replace smtp_username with your Amazon SES SMTP user name.
+$usernameSmtp = process.env.usernameSmtp;
+
+// Replace smtp_password with your Amazon SES SMTP password.
+$passwordSmtp =  process.env.passwordSmtp;
+
+// Specify a configuration set. If you do not want to use a configuration
+// set, comment or remove the next line.
+$configurationSet = 'ConfigSet';
+
+// If you're using Amazon SES in a region other than US West (Oregon),
+// replace email-smtp.us-west-2.amazonaws.com with the Amazon SES SMTP
+// endpoint in the appropriate region.
+$host = 'email-smtp.eu-west-2.amazonaws.com';
+$port = 465;
+
+// The subject line of the email
+$subject = 'Amazon SES test (SMTP interface accessed using PHP)';
+
+// The plain-text body of the email
+$bodyText =  "Email Test\r\nThis email was sent through the
+    Amazon SES SMTP interface using the PHPMailer class.";
+
     if ($_SERVER['REQUEST_METHOD'] === "POST") { 
         if (empty($_POST['contact_email'])) {
             $errors[] = 'Email is empty';
@@ -39,7 +79,7 @@
         if (empty($errors)) {
                     
             $date = date('j, F Y h:i A'); 
-            $emailBody = " <html> 
+            $bodyHtml = " <html> 
                                 <head> 
                                     <title>
                                         $email is contacting you
@@ -66,10 +106,46 @@
             $to = 'info@repotecc.com'; 
             $subject = 'Contact Us'; 
                 
-            if (mail(
-                $to, $subject, $emailBody, $headers)) { 
-                    $sent = true;
-            } 
+           
+            $mail = new PHPMailer(true);
+
+            try {
+                // Specify the SMTP settings.
+                // $sent = true;
+                $mail->isSMTP(true);
+                $mail->setFrom($sender, $senderName);
+                $mail->Username   = $usernameSmtp;
+                $mail->Password   = $passwordSmtp;
+                $mail->Host       = $host;
+                $mail->Port       = $port;
+                $mail->SMTPAuth   = true;
+                $mail->SMTPSecure = 'ssl';
+                // $mail->addCustomHeader('X-SES-CONFIGURATION-SET', $configurationSet);
+            
+                // Specify the message recipients.
+                $mail->addAddress($recipient);
+                // You can also add CC, BCC, and additional To recipients here.
+            
+                // Specify the content of the message.
+                $mail->isHTML(true);
+                $mail->Subject    = $subject;
+                $mail->Body       = $bodyHtml;
+                $mail->AltBody    = $bodyText;
+                $mail->Send();
+                // echo "Email sent!" , PHP_EOL;
+                $sent = true;
+
+            } catch (phpmailerException $e) {
+                echo "An error occurred. {$e->errorMessage()}", PHP_EOL; //Catch errors from PHPMailer.
+            } catch (Exception $e) {
+                echo "Email not sent. {$mail->ErrorInfo}", PHP_EOL; //Catch errors from Amazon SES.
+            }
+           
+           
+            // if (mail(
+            //     $to, $subject, $emailBody, $headers)) { 
+            //         $sent = true;
+            // } 
                 
         } 
     }
---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '085bfc3d46f2ccbe69d5824d9371c3a6ce34cb12',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '085bfc3d46f2ccbe69d5824d9371c3a6ce34cb12',
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.5.3',
+      'version' => '6.5.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'baeb7cde6b60b1286912690ab0693c7789a31e71',
+    ),
+  ),
+);
