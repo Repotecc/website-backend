5c3f1f09e05eedf0b09618d9b5fc2f3d23a93ca8
Repotecc <60607848+Repotecc@users.noreply.github.com>
2022-01-07 01:26:51 +0100

    Update hireusform.php

--- hireusform.php
+++ hireusform.php
@@ -1,83 +1,30 @@
 <?php
 
-// Import PHPMailer classes into the global namespace
-// These must be at the top of your script, not inside a function
-
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
-// If necessary, modify the path in the require statement below to refer to the
-// location of your Composer autoload.php file.
-require 'vendor/autoload.php';
-
-// Replace sender@example.com with your "From" address.
-// This address must be verified with Amazon SES.
-$sender = 'ayomide@repotecc.com';
-$senderName = 'Repotecc';
-
-// Replace recipient@example.com with a "To" address. If your account
-// is still in the sandbox, this address must be verified.
-$recipient = 'info@repotecc.com';
-
-// Replace smtp_username with your Amazon SES SMTP user name.
-$usernameSmtp = process.env.usernameSmtp;
-
-// Replace smtp_password with your Amazon SES SMTP password.
-$passwordSmtp =  process.env.passwordSmtp;
-// Specify a configuration set. If you do not want to use a configuration
-// set, comment or remove the next line.
-$configurationSet = 'ConfigSet';
-
-// If you're using Amazon SES in a region other than US West (Oregon),
-// replace email-smtp.us-west-2.amazonaws.com with the Amazon SES SMTP
-// endpoint in the appropriate region.
-$host = 'email-smtp.eu-west-2.amazonaws.com';
-$port = 465;
-
-// The subject line of the email
-$subject = 'Amazon SES test (SMTP interface accessed using PHP)';
-
-// The plain-text body of the email
-$bodyText =  "Email Test\r\nThis email was sent through the
-    Amazon SES SMTP interface using the PHPMailer class.";
-
-// The HTML-formatted body of the email
-$bodyHtml = '<h1>Email Test</h1>
-    <p>This email was sent through the
-    <a href="https://aws.amazon.com/ses">Amazon SES</a> SMTP
-    interface using the <a href="https://github.com/PHPMailer/PHPMailer">
-    PHPMailer</a> class.</p>';
-
-$mail = new PHPMailer(true);
-
-try {
-    // Specify the SMTP settings.
-    $mail->isSMTP(true);
-    $mail->setFrom($sender, $senderName);
-    $mail->Username   = $usernameSmtp;
-    $mail->Password   = $passwordSmtp;
-    $mail->Host       = $host;
-    $mail->Port       = $port;
-    $mail->SMTPAuth   = true;
-    $mail->SMTPSecure = 'ssl';
-    // $mail->addCustomHeader('X-SES-CONFIGURATION-SET', $configurationSet);
-
-    // Specify the message recipients.
-    $mail->addAddress($recipient);
-    // You can also add CC, BCC, and additional To recipients here.
-
-    // Specify the content of the message.
-    $mail->isHTML(true);
-    $mail->Subject    = $subject;
-    $mail->Body       = $bodyHtml;
-    $mail->AltBody    = $bodyText;
-    $mail->Send();
-    echo "Email sent!" , PHP_EOL;
-} catch (phpmailerException $e) {
-    echo "An error occurred. {$e->errorMessage()}", PHP_EOL; //Catch errors from PHPMailer.
-} catch (Exception $e) {
-    echo "Email not sent. {$mail->ErrorInfo}", PHP_EOL; //Catch errors from Amazon SES.
+function cors() {
+    
+    // Allow from any origin
+    if (isset($_SERVER['HTTP_ORIGIN'])) {
+        // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
+        // you want to allow, and if so:
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 86400');    // cache for 1 day
+    }
+    
+    // Access-Control headers are received during OPTIONS requests
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+        
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+            // may also be using PUT, PATCH, HEAD etc
+            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+        
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+    
+        exit(0);
+    }
+    
+    echo "You have CORS!";
 }
-
 ?>
 
