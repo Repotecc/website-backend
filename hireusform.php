8345d083d5249967f9ec637ee7a38a0d78439948
repotecc <repotecc@gmail.com>
2022-01-13 00:19:52 +0100

    Update hireusform.php

--- hireusform.php
+++ hireusform.php
@@ -50,8 +50,8 @@ $configurationSet = 'ConfigSet';
 // If you're using Amazon SES in a region other than US West (Oregon),
 // replace email-smtp.us-west-2.amazonaws.com with the Amazon SES SMTP
 // endpoint in the appropriate region.
-$host = 'ssl://email-smtp.eu-west-2.amazonaws.com';
-$port = 587;
+$host = 'email-smtp.eu-west-2.amazonaws.com';
+$port = 465;
 
 // The subject line of the email
 $subject = 'Amazon SES test (SMTP interface accessed using PHP)';
